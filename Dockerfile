baf8259de18f615844942a0bb38377c9df312a2f
Oby <obyY94@gmail.com>
2018-07-27 13:17:36 +0200

    Update to version 1.0.80.480

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,13 @@
-FROM ubuntu
+FROM ubuntu:bionic
 
-RUN apt-get update && apt-get install -y --no-install-recommends \
+LABEL maintainer "Obyy https://github.com/obyy/spotify-dockerfile"
+
+ARG SPOTIFY_VERSION="1.0.80.480"
+ARG SPOTIFY_DEB="1:1.0.80.480.g51b03ac3-13"
+ARG DEBIAN_FRONTEND="noninteractive"
+
+
+RUN apt-get -qq update && apt-get -qq install -y --no-install-recommends \
 	dirmngr \
 	gosu \
 	gnupg \
@@ -9,7 +16,7 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
 RUN 	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90 && \
 	echo "deb http://repository.spotify.com stable non-free" > /etc/apt/sources.list.d/spotify.list && \
 	apt-get update && apt-get install -y --no-install-recommends \
- 	spotify-client \
+ 	spotify-client="$SPOTIFY_DEB" \
 	&& echo enable-shm=no >> /etc/pulse/client.conf \
 	&& rm -rf /var/lib/apt/lists/* && apt-get clean
 
---
+++ release.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+#update dockerfile avec la bonne version pour spotify.
+# location of dockerfile need updated
+DOCKERFILE="/home/docky/dockerfile/spotify/Dockerfile"
+
+#URL repository officiel de Spotify
+url="http://repository-origin.spotify.com/pool/non-free/s/spotify-client/"
+
+#need add "1:" for apt-get
+SPOTIFY_DEB="1:$(curl -s $url | sed -rn 's=.*spotify-client_(.*).*_amd64\.deb.*=\1=p' | tail -n1)"
+
+# need tu update git tag.
+SPOTIFY_VERSION=$(echo $SPOTIFY_DEB | sed -rn 's=1:(.*)\..*=\1=p')
+
+# check version of dockerfile
+SPOTIFY_DEB_OLD=$(sed -rn -e 's/ARG SPOTIFY_DEB="(.*)"$/\1/p' $DOCKERFILE)
+
+if [[ "$SPOTIFY_DEB" != "$SPOTIFY_DEB_OLD" ]]; then
+  echo "passage a la version suivante :"
+
+  echo "SPOTIFY_VERSION=$SPOTIFY_VERSION"
+  echo "SPOTIFY_DEB=$SPOTIFY_DEB"
+
+  sed -i -r -e "s/^(ARG SPOTIFY_DEB=).*/\1\"$SPOTIFY_DEB\"/" \
+       -e "s/^(ARG SPOTIFY_VERSION=).*/\1\"$SPOTIFY_VERSION\"/" \
+       $DOCKERFILE
+
+  #mettre les pushs githubs
+git commit -am "Update to version $SPOTIFY_VERSION"
+git tag "$SPOTIFY_VERSION"
+git push --tags
+else
+  echo "Current version : $SPOTIFY_DEB"
+  echo "Dockerfile Version : $SPOTIFY_DEB_OLD"
+  echo "pas de changement de version"
+ exit 0
+fi
+
+exit 0
